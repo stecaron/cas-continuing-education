80e4deb0f047d48ad90a302be71a6a4934e36a15
Stephane Caron <ste.caron@icloud.com>
2018-04-26 08:04:40 -0400

    Add new log UI options

--- continuing-education-app/ui.R
+++ continuing-education-app/ui.R
@@ -10,5 +10,23 @@ ui <- dashboardPage(
       menuItem(text = "Historical logs", tabName = "historical_logs", icon = icon("list-ul"))
     )
   ),
-  dashboardBody()
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "add_log",
+        box(title = "Log informations", width = 6,
+          selectInput("new_log_type_hours", label = "Type of hours", choices = c("Structured", "Unstructured"), selected = "Unstructured"),
+          dateInput("new_log_date", label = "Date of the log", min = "2018-01-01", max = "2021-12-31", value = Sys.Date()),
+          textInput("new_log_location", label = "Location of the log"),
+          textAreaInput("new_log_description", label = "Brief description of the log"),
+          actionButton("new_log_add_button", label = "Save the new log")
+        )
+      ),
+      tabItem(tabName = "stats"
+        
+      ),
+      tabItem(tabName = "historical_logs"
+        
+      )
+    )
+  )
 )
